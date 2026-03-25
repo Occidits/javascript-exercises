aceae2a56daf3ffe04dc91339f666ea9219f22b0
Occidits <180444689+Occidits@users.noreply.github.com>
2026-03-25 11:16:02 +0100

    Finalize exercise 06 from JS foundations

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+	let newStr = "";
+	if (num >= 0) {
+		for (let i = 0; i < num; i++) {
+			newStr += str;
+		}
+	} else {
+		newStr = "ERROR";
+	}
+	return newStr;
 };
 
 // Do not edit below this line
