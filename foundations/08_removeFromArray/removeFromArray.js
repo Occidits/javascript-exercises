8ac750b48c30d0918d66a838e71080f027b66725
Occidits <180444689+Occidits@users.noreply.github.com>
2026-03-25 12:50:53 +0100

    Finalize exercise 08 from JS foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+	let newArr = arr.filter((item) => !num.includes(item));
+	return newArr;
 };
+
+console.log(removeFromArray([2, 5, 1, 7, 0, 3], 5, 7));
 
 // Do not edit below this line
 module.exports = removeFromArray;
