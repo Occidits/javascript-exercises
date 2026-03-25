a6e1d39d6a728c3428d920571599534e8695d392
Occidits <180444689+Occidits@users.noreply.github.com>
2026-03-25 18:25:04 +0100

    Finalize exercise 09 from JS foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+	let min = Math.min(start, end);
+	let max = Math.max(start, end);
 
+	if (
+		min < 0 ||
+		!Number.isInteger(min) ||
+		!Number.isInteger(max) ||
+		typeof start !== "number" ||
+		typeof end !== "number"
+	) {
+		return "ERROR";
+	}
+
+	//Sequence sum formula
+	return ((max - min + 1) * (min + max)) / 2;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
