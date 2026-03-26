b5430f9c5565e97c12adb4fdc7ab0a8b552e2a64
Occidits <180444689+Occidits@users.noreply.github.com>
2026-03-26 09:28:41 +0100

    Finalize exercise 11 from JS Foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+//Conversion formulas
+//x °C ≘ (x × ⁠9/5 + 32) °F - convert Celsius to Fahrenheit
+//x °F ≘ (x − 32) × ⁠5/9⁠ °C - convert Fahrenheit to Celsius
+
+const convertToCelsius = function (num) {
+	let celsiusResult = (num - 32) * (5 / 9);
+
+	//Round to one decimal case
+	return Math.round(celsiusResult * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+	let fahrenheitResult = num * (9 / 5) + 32;
+
+	//Round to one decimal case
+	return Math.round(fahrenheitResult * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
